e933ee71fbdab092c358157edb84ac47d9c43eee
Patrick Michaud <pmichaud@uw.edu>
2012-07-06 09:32:55 -0700

    Moves to using the actual user, does the proper work to register new users with the backend

--- sqlshare/views.py
+++ sqlshare/views.py
@@ -8,12 +8,40 @@ import httplib
 @login_required
 @csrf_protect
 def home(request):
-   return render_to_response('home.html', {})
+   user = request.user
+   return render_to_response('home.html', { "user": user})
 
 @login_required
 @csrf_protect
 def user(request):
-    return HttpResponse('{"schema":"pmichaud","username":"pmichaud"}')
+    conn = httplib.HTTPSConnection(
+        'sqlshare-rest-test.cloudapp.net'
+    )
+    conn.connect()
+    conn.putrequest('GET', '/REST.svc/v1/user/%s' % (request.user))
+    conn.putheader('Accept', 'application/json')
+    conn.endheaders()
+    response = conn.getresponse()
+
+    code = response.status
+    content = response.read()
+
+    if code == 404:
+        conn.putrequest('PUT', '/REST.svc/v1/user/%s' % (request.user))
+        conn.putheader('Accept', 'application/json')
+        conn.putheader('Content-Type', 'application/json')
+        conn.putheader('Content-Length', '0')
+        conn.endheaders()
+        response = conn.getresponse()
+        code = response.status
+        content = response.read()
+
+
+    user = request.user
+    user_response = HttpResponse(content)
+    user_response.status_code = code
+
+    return user_response
 
 @login_required
 @csrf_protect
@@ -25,7 +53,7 @@ def proxy(request, path):
 
     sqlshare_secret = settings.SQLSHARE_SECRET
     conn.putrequest(request.META['REQUEST_METHOD'], '/'+path)
-    conn.putheader('Authorization', 'ss_trust pmichaud : %s' % (sqlshare_secret))
+    conn.putheader('Authorization', 'ss_trust %s : %s' % (request.user, sqlshare_secret))
     conn.putheader('Accept', 'application/json')
     conn.putheader('Content-type', 'application/json')
     conn.endheaders()
