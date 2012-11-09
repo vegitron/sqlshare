84935f109abcd31525fe95438f8c5cc24e058e8c
Patrick Michaud <pmichaud@uw.edu>
2012-11-08 17:29:26 -0800

    Updating the yui_config to know about static/ - gets to the last step in uploading

--- sqlshare/static/javascript/yui_config.js
+++ sqlshare/static/javascript/yui_config.js
@@ -1,6 +1,6 @@
 var YUI_config =  {
-    base: solstice_document_base + "/javascript/yui/3.3.0/build/",
-    comboBase: solstice_document_base + 'javascript/yui/',
+    base: solstice_document_base + "/static/javascript/yui/3.3.0/build/",
+    comboBase: solstice_document_base + 'static/javascript/yui/',
     combine: false
 };
 
