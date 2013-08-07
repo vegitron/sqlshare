ff9202a519133c3419cf954bfea717ea8c1682e3
Patrick Michaud <pmichaud@uw.edu>
2013-08-07 14:25:40 -0700

    Shows server and local data in the permissions server - issue #1

--- sqlshare/models.py
+++ sqlshare/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from sqlshare.utils import _send_request
 from django.utils import simplejson as json
+from django.contrib.auth.models import User
 import urllib
 
 # Create your models here.
@@ -15,7 +16,6 @@ class Dataset(models.Model):
     def get_all_access(self):
         server_data = self._get_server_data()
 
-        local_data = DatasetEmailAccess.objects.filter(dataset = self, is_active = True)
 
         data = {
             'is_shared': server_data['is_shared'],
@@ -23,6 +23,27 @@ class Dataset(models.Model):
             'emails': [],
             'accounts': [],
         }
+
+        stored_dataset_query = Dataset.objects.filter(schema = self.schema, name = self.name)
+
+        if len(stored_dataset_query):
+            stored_dataset = stored_dataset_query[0]
+            local_data = DatasetEmailAccess.objects.filter(dataset = stored_dataset, is_active = True)
+
+            for entry in local_data:
+                data['emails'].append(entry.email)
+
+
+        for username in server_data["authorized_viewers"]:
+            person = User.objects.get_or_create(username = username)[0]
+
+            data['accounts'].append({
+                'login': username,
+                'name': person.first_name,
+                'surname': person.last_name,
+                'origin_email': person.email,
+            })
+
 
         return data
 
